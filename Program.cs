cd3972404c0d0f4533d022528e6045e38922a99e
kaiorod <caiorodrigo2775@gmail.com>
2021-11-21 15:50:52 -0300

    Finaliza Patinhos

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+
+namespace Patinhos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtdpatinhos,c;
+
+            Console.Clear();
+            Console.Beep();
+
+            inicio:
+            Console.Write("Quantos patinhos? : ");
+            qtdpatinhos = int.Parse(Console.ReadLine());
+
+            c = qtdpatinhos;
+
+            if(c < 2)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Digite pelo menos 2 patinhos");
+                Console.ResetColor();
+                goto inicio;
+            }
+            else
+            {
+                while(c>=1)
+                {
+                    if(c > 2)
+                    {
+                        Thread.Sleep(1000);
+                        Console.WriteLine($"\n{c} patinhos foram passear " +
+                        "\nAlém das montanhas Para brincar " +
+                        "\nA mamãe gritou: Quá, quá, quá, quá " +
+                        $"\nMas só {c-1} patinhos voltaram de lá. ");
+                    }
+
+                    else if(c == 2)
+                    {
+                        Thread.Sleep(1000);
+                        Console.WriteLine($"\n{c} patinhos foram passear " +
+                        "\nAlém das montanhas Para brincar " +
+                        "\nA mamãe gritou: Quá, quá, quá, quá " +
+                        $"\nMas só {c-1} patinho voltou de lá. ");
+                    }
+                    else if(c == 1)
+                    {
+                        Thread.Sleep(1000);
+                        Console.WriteLine($"\n{c} patinho foi passear " +
+                        "\nAlém das montanhas Para brincar " +
+                        "\nA mamãe gritou: Quá, quá, quá, quá " +
+                        $"\nMas nenhum patinho voltou de lá. ");
+                    }
+                c--;
+                }
+                Thread.Sleep(1000);
+                Console.WriteLine("\nA mamãe patinha foi procurar" +
+                "\nAlém das montanhas Na beira do mar" +
+                "\nA mamãe gritou: Quá, quá, quá, quá" +
+                $"\nE os {qtdpatinhos} patinhos voltaram de lá.");
+            }
+            
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # Patinhos
-Exiba a letra da música "Cinco Patinhos" (versão brasileira gravada pela Xuxa de "Five little ducks" - The Wiggles). Seu programa deve perguntar quantos patinhos o usuário deseja (pelo menos 2).  A canção começa assim:  (N) patinhos foram passear Além das montanhas Para brincar A mamãe gritou: Quá, quá, quá, quá Mas só (N-1) patinhos voltaram de lá. Esse bloco se repete até nenhum patinho voltar. Ao final, todos os patinhos voltam:  A mamãe patinha foi procurar Além das montanhas Na beira do mar A mamãe gritou: Quá, quá, quá, quá E os (N) patinhos voltaram de lá. Ex.:  Quantos patinhos? 3  3 patinhos foram passear Além das montanhas Para brincar A mamãe gritou: Quá, quá, quá, quá Mas só 2 patinhos voltaram de lá.  2 patinhos foram passear Além das montanhas Para brincar A mamãe gritou: Quá, quá, quá, quá Mas só 1 patinho voltou de lá.  1 patinho foi passear Além das montanhas Para brincar A mamãe gritou: Quá, quá, quá, quá Mas nenhum patinho voltou de lá.  A mamãe patinha foi procurar Além das montanhas Na beira do mar A mamãe gritou: Quá, quá, quá, quá E os 3 patinhos voltaram de lá.
+Exiba a letra da música "Cinco Patinhos" (versão brasileira gravada pela Xuxa de "Five little ducks" - The Wiggles). 
+Seu programa deve perguntar quantos patinhos o usuário deseja (pelo menos 2).  A canção começa assim:  
+(N) patinhos foram passear 
+Além das montanhas Para brincar 
+A mamãe gritou: Quá, quá, quá, quá 
+Mas só (N-1) patinhos voltaram de lá. 
+
+Esse bloco se repete até nenhum patinho voltar. 
+Ao final, todos os patinhos voltam:  
+A mamãe patinha foi procurar 
+Além das montanhas Na beira do mar 
+A mamãe gritou: Quá, quá, quá, quá 
+E os (N) patinhos voltaram de lá. 
+
+Ex.: 
+Quantos patinhos? 3  
+3 patinhos foram passear 
+Além das montanhas Para brincar 
+A mamãe gritou: Quá, quá, quá, quá 
+Mas só 2 patinhos voltaram de lá.  
+
+2 patinhos foram passear 
+Além das montanhas Para brincar 
+A mamãe gritou: Quá, quá, quá, quá 
+Mas só 1 patinho voltou de lá.  
+
+1 patinho foi passear 
+Além das montanhas Para brincar 
+A mamãe gritou: Quá, quá, quá, quá 
+Mas nenhum patinho voltou de lá.  
+
+A mamãe patinha foi procurar 
+Além das montanhas Na beira do mar 
+A mamãe gritou: Quá, quá, quá, quá 
+E os 3 patinhos voltaram de lá.
